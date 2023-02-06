7105a054699eb76635972125242abd41fda05051
chaosannals <chaosannals@outlook.com>
2023-02-06 10:57:30 +0800

    save

---
+++ crackcpp/crackwa/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+INT WINAPI wWinMain(
+    _In_ HINSTANCE instance,
+    _In_opt_ HINSTANCE prevInstance,
+    _In_ LPWSTR    cmdLine,
+    _In_ int       cmdShow) {
+
+    return 0;
+}
---
+++ crackcpp/readme.md
@@ -0,0 +1,1 @@
+# CrackMe Cpp maker
