211a81a29c87d625132c1cc7d3dcd7f8a2cd47e5
chenshenchao <chenshenchao@outlook.com>
2023-03-07 01:57:26 +0800

    save

--- wntdbg/buger/src/main.cpp
+++ wntdbg/buger/src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <Windows.h>
+
+void EchoSysInfo() {
+	DWORD computer_name_length = MAX_COMPUTERNAME_LENGTH;
+	WCHAR computer_name[MAX_COMPUTERNAME_LENGTH];
+	if (GetComputerNameW(computer_name, &computer_name_length)) {
+		std::wcout << "计算机名:" << computer_name_length << computer_name << std::endl;
+	}
+	else {
+		std::cout << "获取计算机名失败" << std::endl;
+	}
+
+	SYSTEM_INFO si = { 0 };
+	GetSystemInfo(&si);
+	std::wcout << "CPU 数量:" << si.dwNumberOfProcessors << std::endl;
+	std::wcout << "CPU 类型：" << si.dwProcessorType << std::endl;
+}
 
 int main() {
 	std::cout << "start" << std::endl;
+	EchoSysInfo();
 	std::cin.get();
 	std::cout << "end" << std::endl;
 	return 0;
--- wntdbg/dbginject/src/main.cpp
+++ wntdbg/dbginject/src/main.cpp
@@ -1,25 +1,7 @@
 #include <Windows.h>
 #include <iostream>
 
-void EchoSysInfo() {
-	DWORD computer_name_length = MAX_COMPUTERNAME_LENGTH;
-	WCHAR computer_name[MAX_COMPUTERNAME_LENGTH];
-	if (GetComputerNameW(computer_name, &computer_name_length)) {
-		// computer_name[computer_name_length - 1] = '\0';
-		std::wcout << "计算机名:" << computer_name_length << computer_name << std::endl;
-	}
-	else {
-		std::cout << "获取计算机名失败" << std::endl;
-	}
-
-	SYSTEM_INFO si = { 0 };
-	GetSystemInfo(&si);
-	std::wcout << "CPU 数量:" << si.dwNumberOfProcessors << std::endl;
-	std::wcout << "CPU 类型：" << si.dwProcessorType << std::endl;
-}
-
 int main() {
-	EchoSysInfo();
 
 	std::cin.get();
 	return 0;
